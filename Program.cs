758b9476cffae2cc15fcb816bc6e7b4b845ffa84
Erik Gúth <guthember@gmail.com>
2019-09-11 16:29:14 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      String firstName;
+      String lastName;
+
+      Console.Write("What is your first name: ");
+      firstName = Console.ReadLine();
+
+      Console.Write("What is your last name: ");
+      lastName = Console.ReadLine();
+
+      Console.WriteLine("Hello " + firstName + " " + lastName);
+
+
+      Console.ReadKey();
+
+    }
+  }
+}
